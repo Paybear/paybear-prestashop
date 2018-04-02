6e931293846b54c68b2ee9abcf2c5e464d13c08c
Paul Shatalov <leviothano@gmail.com>
2018-04-02 23:27:48 +0700

    bump version

--- paybear.php
+++ paybear.php
@@ -25,7 +25,7 @@ class PayBear extends PaymentModule
     {
         $this->name = 'paybear';
         $this->tab = 'payments_gateways';
-        $this->version = '0.7.3';
+        $this->version = '0.7.4';
         $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
         $this->author = 'PayBear';
         $this->controllers = array('validation', 'currencies', 'payment', 'callback', 'status');
